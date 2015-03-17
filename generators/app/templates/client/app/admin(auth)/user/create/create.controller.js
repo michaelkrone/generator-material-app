4e3ed34c736606bceab4544982a228234702126b
Michael Krone <mk@gastro-instruments.de>
2015-03-17 14:54:02 +0100

    feat: finished user route

--- generators/app/templates/client/app/admin(auth)/user/create/create.controller.js
+++ generators/app/templates/client/app/admin(auth)/user/create/create.controller.js
@@ -1,41 +1,94 @@
+/**
+ * @ngdoc controller
+ * @name <%= scriptAppName %>.admin.user.create.controller:UserCreateController
+ * @description
+ * Controller of the user create page of the admin section
+ */
+
 (function () {
 	'use strict';
 
 	/**
-	 * Register the edit controller as UserCreateController
- 	 */
+	 * Register the create controller as UserCreateController
+	 */
 
 	angular
 		.module('<%= scriptAppName %>.admin.user.create')
 		.controller('UserCreateController', UserCreateController);
 
-	// add UserCreateController dependencies to inject
-	UserCreateController.$inject = ['Auth', 'User', '$state', '$mdToast', '$mdDialog'];
-
 	/**
-	 * UserCreateController constructor
+	 * @ngdoc function
+	 * @name <%= scriptAppName %>.admin.user.create.provider:UserCreateController
+	 * @description
+	 * Provider of the {@link <%= scriptAppName %>.admin.user.create.controller:UserCreateController UserCreateController}
+	 *
+	 * @param {Service} Auth The Auth service to use
+	 * @param {Service} User The User service to use
+	 * @param {Service} Toast The Toast service to use
+	 * @param {Service} $mdDialog The mdDialog service to use
+	 * @returns {Service} {@link <%= scriptAppName %>.admin.user.create.controller:UserCreateController UserCreateController}
 	 */
-	function UserCreateController(Auth, User, $state, $mdToast, $mdDialog) {
+
+	UserCreateController.$inject = ['$mdDialog', 'Auth', 'User', 'Toast'];
+
+	function UserCreateController($mdDialog, Auth, User, Toast) {
 		var vm = this;
 
-		// defaults
-		vm.message = {};
-		vm.errors = {};
-		vm.user = new User();
+		/**
+		 * @ngdoc property
+		 * @name userRoles
+		 * @propertyOf <%= scriptAppName %>.admin.user.create.controller:UserCreateController
+		 * @description
+		 * The settable user roles
+		 *
+		 * Default retrieved from {@link auth.service:Auth#getSettableRoles Auth.getSettableRoles()}
+		 * @returns {Array} settable user roles
+		 */
+		vm.userRoles = Auth.getSettableRoles();
 
-		// view model bindings
-		vm.create = create;
-		vm.showToast = showToast;
-		vm.openEdit = openEdit;
-		vm.hideDialog = hideDialog;
-		vm.cancelDialog = cancelDialog;
-
-		// view model implementations
 
 		/**
-		 * Create a new user by using the UserService create method
+		 * @ngdoc function
+		 * @name getNameForRole
+		 * @propertyOf <%= scriptAppName %>.admin.user.create.controller:UserCreateController
+		 * @description
+		 * Function for returning the human readable name of the passed user role
+		 *
+		 * Default retrieved from {@link auth.service:Auth#getNameForRole Auth.getNameForRole()}
+		 * @param {String} rolename The name of the user role
+		 * @returns {String} Name of the user role
 		 */
-		function create(form) {
+		vm.getNameForRole = Auth.getNameForRole;
+
+		/**
+		 * @ngdoc property
+		 * @name user
+		 * @propertyOf <%= scriptAppName %>.admin.user.create.controller:UserCreateController
+		 * @description
+		 * The new user data
+		 *
+		 * Defaults:
+		 * - active	=	true
+		 * - role	=	user
+		 * @returns {User} User data
+		 */
+		vm.user = new User({active: true, role: 'user'});
+
+		// view model bindings (documented below)
+		vm.create = createUser;
+		vm.close = hideDialog;
+		vm.cancel = cancelDialog;
+
+		/**
+		 * @ngdoc function
+		 * @name create
+		 * @methodOf <%= scriptAppName %>.admin.user.create.controller:UserCreateController
+		 * @description
+		 * Create a new user by using the UserService create method
+		 *
+		 * @param {form} [form] The form to gather the information from
+		 */
+		function createUser(form) {
 			// refuse to work with invalid data
 			if (vm.user._id || (form && !form.$valid)) {
 				return;
@@ -43,65 +96,49 @@
 
 			Auth.createUser(vm.user)
 				.then(createUserSuccess)
-				.catch(catchUserCreateErrors);
+				.catch(createUserCatch);
 
 			function createUserSuccess(newUser) {
-				vm.user = newUser;
-				vm.message = {type: 'success', message: 'user created'};
-				(form && form.$setPristine());
-				vm.hideDialog();
-				vm.showToast();
+				Toast.show({
+					type: 'success',
+					text: 'User ' + newUser.name + ' has been created',
+					link: {state: 'admin.user.list.detail', params: {id: newUser._id}}
+				});
+				vm.close();
 			}
 
-			function catchUserCreateErrors(err) {
-				err = err.data || err;
-				vm.message = {type: 'danger', message: 'Error while saving user: ' + err.toString() };
-				resetErrors(err, form);
-				vm.showToast();
+			function createUserCatch(err) {
+				if (form && err) {
+					form.setResponseErrors(err);
+				}
+
+				Toast.show({
+					type: 'warn',
+					text: 'Error while creating a new user'
+				});
 			}
 		}
 
 		/**
+		 * @ngdoc function
+		 * @name hide
+		 * @methodOf <%= scriptAppName %>.admin.user.create.controller:UserCreateController
+		 * @description
 		 * Hide the dialog
 		 */
 		function hideDialog() {
 			$mdDialog.hide();
 		}
 
-		/***
+		/**
+		 * @ngdoc function
+		 * @name cancel
+		 * @methodOf <%= scriptAppName %>.admin.user.create.controller:UserCreateController
+		 * @description
 		 * Cancel the dialog
 		 */
 		function cancelDialog() {
 			$mdDialog.cancel();
-		}
-
-		/**
-		 * Open the detail state with the current user
-		 */
-		function openEdit() {
-			$state.go('^.edit', {'id': vm.user._id});
-		}
-
-		/**
-		 * Update validity of form fields that match the mongoose errors
-		 */
-		function resetErrors(err, form) {
-			angular.forEach(err.errors, function (error, field) {
-				(form && form[field]).$setValidity('mongoose', false);
-				vm.errors[field] = error.type;
-			});
-		}
-
-		/**
-		 * Display a toast with the given content. If the content is falsy
-		 * use vm.message.message instead.
-		 *
-		 * @param {String} [content] - The toasts content
-		 * @param {$event} [$event] - unused, may be passed to the service
-		 */
-		function showToast(content, $event) {
-			content = content || vm.message.message;
-			$mdToast.show($mdToast.simple().content(content));
 		}
 	}
 })();

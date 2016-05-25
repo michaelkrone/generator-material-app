56573ecc157b5ce9deb90417765cfd8be5f4781d
stupid <sunfeng32@qq.com>
2016-05-25 11:33:39 +0800

    feat(convertDateStringsToDates): friendly feature

--- generators/app/templates/client/app/app.js
+++ generators/app/templates/client/app/app.js
@@ -32,6 +32,8 @@
     $urlRouterProvider.otherwise('/');
     $urlMatcherFactoryProvider.strictMode(false);
     $locationProvider.html5Mode(true);
+
+    $httpProvider.defaults.transformResponse.push(convertDateStringsToDates);
   <% if(features.auth) { %>
     $httpProvider.interceptors.push('AuthInterceptor');<% } %>
 
@@ -85,5 +87,29 @@
       });
     });
   }<% } %>;
+
+  var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d{1,})(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
+  function convertDateStringsToDates(input) {
+    // Ignore things that aren't objects.
+    if (typeof input !== "object") return input;
+
+    for (var key in input) {
+      if (!input.hasOwnProperty(key)) continue;
+
+      var value = input[key];
+      var match;
+      // Check for string properties which look like dates.
+      if (typeof value === "string" && (match = value.match(regexIso8601))) {
+        var milliseconds = Date.parse(match[0])
+        if (!isNaN(milliseconds)) {
+          input[key] = new Date(milliseconds);
+        }
+      } else if (typeof value === "object") {
+        // Recurse into object
+        convertDateStringsToDates(value);
+      }
+    }
+    return input;
+  }
 
 })();

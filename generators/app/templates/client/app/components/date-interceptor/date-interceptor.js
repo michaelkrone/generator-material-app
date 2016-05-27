051de1ea5f5addb1d1df4308e5eaba342fe9576a
stupid <sunfeng32@qq.com>
2016-05-27 21:27:52 +0800

    fix(DateInterceptor): use moment and add format

--- generators/app/templates/client/app/components/date-interceptor/date-interceptor.js
+++ generators/app/templates/client/app/components/date-interceptor/date-interceptor.js
@@ -1,6 +1,6 @@
 (function () {
   'use strict';
-  var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d{1,})(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
+  var minimumDatePart = 'YYYY-MM-DDTHH:mm:ss.SSSZZ';
 
   // register the service as DateInterceptorService
   angular
@@ -23,13 +23,10 @@
       if (!input.hasOwnProperty(key)) continue;
 
       var value = input[key];
-      var match;
+      var dateTime = moment(value, minimumDatePart);
       // Check for string properties which look like dates.
-      if (angular.isString(value) && (match = value.match(regexIso8601))) {
-        var milliseconds = Date.parse(match[0])
-        if (!isNaN(milliseconds)) {
-          input[key] = new Date(milliseconds);
-        }
+      if (angular.isString(value) && dateTime.isValid()) {
+        input[key] = dateTime.toDate();
       } else if (angular.isObject(value)) {
         // Recurse into object
         DateInterceptor(value);
